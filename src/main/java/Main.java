0e28039406f7da814f0fa5db194a46dd6f772eb5
Devivdb <devivdb@gmail.com>
2025-04-08 20:46:10 +0200

    Uitwerking

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
+        Customer customer = new Customer("Devi", "Hams", new MasterCard(0));
+        customer.getCreditCard().pay(100);
+        customer.printName();
+        System.out.println(customer.getCreditCard().getDebt());
+
+
+        Customer customer2 = new Customer("Bale", "Ling", new VisaCard(0));
+        customer2.getCreditCard().pay(100);
+
+        customer2.printName();
+        System.out.println(customer2.getCreditCard().getDebt());
 
     }
 }
